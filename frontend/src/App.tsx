6908eaf0647b916fe2e0965152aa8ecfbfdf9b00
sohanshrestha12 <sohanshrestha40@gmail.com>
2024-07-29 23:54:20 +0545

    Register and login completed

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
 import Layout from './layouts/Layout'
 import Register from './pages/Register'
+import SignIn from './pages/SignIn'
 
 const App = () => {
   return (
@@ -9,7 +10,8 @@ const App = () => {
         <Route path="/" element={<Layout><p>Home Page</p></Layout>}/>
         <Route path="/search" element={<Layout>Search Page</Layout>}/>
         <Route path='*' element={<Navigate to="/"/>}/>
-        <Route path='/register' element={<Layout><Register/></Layout>}/>  
+        <Route path='/register' element={<Layout><Register/></Layout>}/> 
+        <Route path='/sign-in' element={<Layout><SignIn/></Layout>} /> 
       </Routes>
     </BrowserRouter>
   )
--- frontend/src/pages/Register.tsx
+++ frontend/src/pages/Register.tsx
@@ -1,4 +1,8 @@
 import { useForm } from "react-hook-form";
+import { useMutation, useQueryClient } from "react-query";
+import * as apiClient from "../api-client";
+import { useAppContext } from "../contexts/AppContext";
+import { Link, useNavigate } from "react-router-dom";
 
 export type RegisterFormData = {
   firstName: string;
@@ -9,11 +13,30 @@ export type RegisterFormData = {
 };
 
 const Register = () => {
-  const { register,watch,handleSubmit,formState:{errors} } = useForm<RegisterFormData>();
+  const queryClient = useQueryClient();
+  const { showToast } = useAppContext();
+  const navigate = useNavigate();
+  const {
+    register,
+    watch,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<RegisterFormData>();
 
-  const onSubmit=handleSubmit((data)=>{
-    console.log(data);
-  })
+  const mutation = useMutation(apiClient.register, {
+    onSuccess: async () => {
+      showToast({ message: "Registration Success!", type: "SUCCESS" });
+      await queryClient.invalidateQueries("validateToken");
+      navigate("/");
+    },
+    onError: (error: Error) => {
+      showToast({ message: error.message, type: "ERROR" });
+    },
+  });
+
+  const onSubmit = handleSubmit((data) => {
+    mutation.mutate(data);
+  });
   return (
     <form className="flex flex-col gap-5" onSubmit={onSubmit}>
       <h2 className="text-3xl font-bold">Create an Account</h2>
@@ -24,9 +47,9 @@ const Register = () => {
             className="border rounded w-full py-1 px-2 font-normal"
             {...register("firstName", { required: "This Field is required" })}
           ></input>
-          {errors.firstName && 
+          {errors.firstName && (
             <span className="text-red-500">{errors.firstName.message}</span>
-          }
+          )}
         </label>
         <label className="text-gray-700 text-sm font-bold flex-1">
           Last Name
@@ -34,9 +57,9 @@ const Register = () => {
             className="border rounded w-full py-1 px-2 font-normal"
             {...register("lastName", { required: "This Field is required" })}
           />
-            {errors.lastName && 
+          {errors.lastName && (
             <span className="text-red-500">{errors.lastName.message}</span>
-          }
+          )}
         </label>
       </div>
       <label className="text-gray-700 text-sm font-bold flex-1">
@@ -46,39 +69,54 @@ const Register = () => {
           className="border rounded w-full py-1 px-2 font-normal"
           {...register("email", { required: "This Field is required" })}
         />
-          {errors.email && 
-            <span className="text-red-500">{errors.email.message}</span>
-          }
+        {errors.email && (
+          <span className="text-red-500">{errors.email.message}</span>
+        )}
       </label>
       <label className="text-gray-700 text-sm font-bold flex-1">
         Password
         <input
           type="password"
           className="border rounded w-full py-1 px-2 font-normal"
-          {...register("password", { required: "This Field is required",minLength:{value:6,message:"Password must be at least 6 characters"} })}
+          {...register("password", {
+            required: "This Field is required",
+            minLength: {
+              value: 6,
+              message: "Password must be at least 6 characters",
+            },
+          })}
         />
-          {errors.password && 
-            <span className="text-red-500">{errors.password.message}</span>
-          }
+        {errors.password && (
+          <span className="text-red-500">{errors.password.message}</span>
+        )}
       </label>
       <label className="text-gray-700 text-sm font-bold flex-1">
         Confirm Password
         <input
           type="password"
           className="border rounded w-full py-1 px-2 font-normal"
-          {...register("confirmPassword", {validate:(val)=>{
-            if(!val){
+          {...register("confirmPassword", {
+            validate: (val) => {
+              if (!val) {
                 return "This field is required";
-            }else if(watch("password")!==val){
+              } else if (watch("password") !== val) {
                 return "Your passwords do not match";
-            }
-          }})}
+              }
+            },
+          })}
         />
-          {errors.confirmPassword && 
-            <span className="text-red-500">{errors.confirmPassword.message}</span>
-          }
+        {errors.confirmPassword && (
+          <span className="text-red-500">{errors.confirmPassword.message}</span>
+        )}
       </label>
-      <span>
+      <span className="flex items-center justify-between">
+        <span className="text-sm">
+          Alredy Registered?{" "}
+          <Link to="/sign-in" className="underline text-blue-600">
+            Login to your Account
+          </Link>
+        </span>
+
         <button
           type="submit"
           className="bg-blue-600 text-white p-2 font-bold hover:bg-blue-500 text-xl"
