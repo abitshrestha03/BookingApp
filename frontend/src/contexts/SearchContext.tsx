74f8c9cab5e5b2a820c55537064134c367a1b9d8
sohanshrestha12 <sohanshrestha40@gmail.com>
2024-08-04 01:01:52 +0545

    session storage for search

--- frontend/src/contexts/SearchContext.tsx
+++ frontend/src/contexts/SearchContext.tsx
@@ -25,12 +25,26 @@ const SearchContext = React.createContext<SearchContext | undefined>(undefined);
 export const SearchContextProvider = ({
   children,
 }: SearchContextProviderProps) => {
-  const [destination, setDestination] = useState<string>("");
-  const [checkIn, setCheckIn] = useState<Date>(new Date());
-  const [checkOut, setCheckOut] = useState<Date>(new Date());
-  const [adultCount, setAdultCount] = useState<number>(1);
-  const [childCount, setChildCount] = useState<number>(0);
-  const [hotelId, setHotelId] = useState<string>("");
+  const [destination, setDestination] = useState<string>(
+    () => sessionStorage.getItem("destination") || ""
+  );
+  const [checkIn, setCheckIn] = useState<Date>(
+    () =>
+      new Date(sessionStorage.getItem("checkIn") || new Date().toISOString())
+  );
+  const [checkOut, setCheckOut] = useState<Date>(
+    () =>
+      new Date(sessionStorage.getItem("checkOut") || new Date().toISOString())
+  );
+  const [adultCount, setAdultCount] = useState<number>(() =>
+    parseInt(sessionStorage.getItem("adultCount") || "1")
+  );
+  const [childCount, setChildCount] = useState<number>(() =>
+    parseInt(sessionStorage.getItem("childCount") || "0")
+  );
+  const [hotelId, setHotelId] = useState<string>(
+    () => sessionStorage.getItem("hotelId") || ""
+  );
 
   const saveSearchValues = (
     destination: string,
@@ -38,16 +52,26 @@ export const SearchContextProvider = ({
     checkOut: Date,
     adultCount: number,
     childCount: number,
-    hotelId?:string,
+    hotelId?: string
   ) => {
     setDestination(destination);
     setCheckIn(checkIn);
     setCheckOut(checkOut);
     setAdultCount(adultCount);
     setChildCount(childCount);
-    if(hotelId){
-        setHotelId(hotelId);
+    if (hotelId) {
+      setHotelId(hotelId);
     }
+    sessionStorage.setItem("destination",destination);
+    sessionStorage.setItem("checkIn",checkIn.toISOString());
+    sessionStorage.setItem("checkOut",checkOut.toISOString());
+    sessionStorage.setItem("adultCount",adultCount.toString());
+    sessionStorage.setItem("childCount",childCount.toString());
+
+    if(hotelId){
+      sessionStorage.setItem("hotelId",hotelId);
+    }
+
   };
 
   return (
@@ -59,7 +83,7 @@ export const SearchContextProvider = ({
         adultCount,
         childCount,
         saveSearchValues,
-        hotelId
+        hotelId,
       }}
     >
       {children}
@@ -67,7 +91,7 @@ export const SearchContextProvider = ({
   );
 };
 
-export const useSearchContext = () =>{
-    const context = useContext(SearchContext);
-    return context as SearchContext;
-}
+export const useSearchContext = () => {
+  const context = useContext(SearchContext);
+  return context as SearchContext;
+};
